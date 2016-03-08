07eb40524dc9063932145d4bb2d33c2a9e2e20c9
Anna Lezhikova <anna.v.l@gmail.com>
2016-03-08 20:20:07 +1300

    Draw nodes start at scale 10, zoom works

--- MapDrawer.java
+++ MapDrawer.java
@@ -1,43 +1,55 @@
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MapDrawer extends GUI{
 	private  List<Node> nodeCollection;
+	private int scale;
 	
 	public  MapDrawer () {
 		nodeCollection = new ArrayList<Node>();
+		scale = 800;
 	}
 
 
 	@Override
 	protected void redraw(Graphics g) {
-		for (Node node : nodeCollection)
+		for (Node node : nodeCollection) {
+			node.setScale(scale);
 			node.draw(g);		
+		}
 	}
 
 	@Override
 	protected void onClick(MouseEvent e) {
-		// TODO Auto-generated method stub
+		System.out.print(e);
 		
 	}
 
 	@Override
 	protected void onSearch() {
-		// TODO Auto-generated method stub
+
 		
 	}
 
 	@Override
 	protected void onMove(Move m) {
-		// TODO Auto-generated method stub
+		if (m == Move.ZOOM_IN) {
+			zoom(10);
+		} else if (m == Move.ZOOM_OUT) {
+			zoom(-10);
+		}
 		
 	}
+
+	private void zoom(int step) {
+		scale = scale + step;
+		System.out.println(scale);
+	}
+
 
 	@Override
 	protected void onLoad(File nodesFile, File roads, File segments, File polygons) {
--- Node.java
+++ Node.java
@@ -1,11 +1,11 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
-
-//import java.awt.Graphics;
 
 public class Node {
 	private final int id;
 	private final Location loc;
+	private int scale;
 	
 	public Node (String input){
 		String [] parts = input.split("\\s+");
@@ -14,8 +14,12 @@ public class Node {
 		double lat = Double.parseDouble(parts[1]);
 		double lon = Double.parseDouble(parts[2]);
 		loc = Location.newFromLatLon(lat, lon);
+//		scale = 800;
 	}
 	
+	public void setScale(int scale) {
+		this.scale = scale;
+	}
 	public void draw(Graphics g) {
 ////		g.setColor(color);
 //	public Point asPoint(Location origin, double scale) {
@@ -23,9 +27,10 @@ public class Node {
 //		int v = (int) ((origin.y - y) * scale);
 //		return new Point(u, v);
 //	}	
-		Location origin = new Location(-50,-50);
-		Point nodeLoc = loc.asPoint(origin, 1);
-		g.drawRect(nodeLoc.x, nodeLoc.y, 0, 0);
+		Location origin = new Location(0,0);
+		Point nodeLoc = loc.asPoint(origin, scale);
+		g.setColor(new Color(  0,   0, 255));
+		g.fillRect(nodeLoc.x, nodeLoc.y, 2, 2);
 	}
 	public String toString() {
 		return "NodeId: " + this.id + ", Lat: " + this.loc.x + ", Lon: " + this.loc.y;
